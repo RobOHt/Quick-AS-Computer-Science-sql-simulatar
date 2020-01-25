a1fd1790182fb8a26627649394381ecd5d089994
RobOHt <32957875+RobOHt@users.noreply.github.com>
2020-01-26 01:04:31 +0800

    Init with skeletons
    Lacks requirements.txt and refinements.

---
+++ Trainsqlite3.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+with sqlite3.connect("database.db") as connection:
+    c = connection.cursor()
+    try:
+        c.execute("""CREATE TABLE img_choice(image TEXT, A TEXT, B TEXT, C TEXT, D TEXT)""")
+    except sqlite3.OperationalError:
+        pass
